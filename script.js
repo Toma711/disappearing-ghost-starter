3bbaafc0ca3864a30fd0a561f0f0b036d141054c
Glitch (cool-ghost-man) <none>
2020-11-11 01:54:26 +0000

    👵👩 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+$(".hide-button").click(function() {
+  $(".ghost-image").hide();
+});
 
+$(".show-button").click(function() {
+  $(".ghost-image").show();
+});
+
+$(".update-img-button").click(function() {
+  $(".ghost-image").attr("src", "https://data.whicdn.com/images/124172397/original.jpg");
+});
+
+$(".message-button").click(function() {
+  let message = $("input").val();
+  $(".message").append(message + "\n");
+});
